601481f9787407220c9b0406a1a77b8135903721
matteocianf <matteocianfa@gmail.com>
2025-05-23 11:58:30 +0200

    Update losito_runner.py

--- losito_runner.py
+++ losito_runner.py
@@ -56,12 +56,12 @@ losito_run = f'losito ../losito.parset'
 print(losito_run)
 os.system(losito_run)
 
-freq_avg = f'DP3 ../dp3_freqavg.parset'
-print(freq_avg)
-os.system(freq_avg)
-
 single_ms = f'DP3 msin={name}*.MS msout={name}.MS msout.storemanager=dysco'
 print(single_ms)
 os.system(single_ms)
+
+freq_avg = f'DP3 ../dp3_freqavg.parset'
+print(freq_avg)
+os.system(freq_avg)
 
 os.chdir(dir_work)
